6546eb1268337995b0d999cb6d7be29eba37ade3
Annapurna jadhav <jadhavannapurna37@gmail.com>
2026-01-08 16:06:23 +0530

    fixed env issues

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,16 +1,15 @@
-import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require("../keys/service.json");
+import admin from "firebase-admin";
+
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+  );
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
-
-
-
 
 export default admin;
--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,13 @@
 
-import dotenv from "dotenv";
-dotenv.config({ path: './.env' });
-
+import "./env.js"
 import {app} from "./app.js";
-
 
 
 const PORT = process.env.PORT||8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+ 
 });
 
 
